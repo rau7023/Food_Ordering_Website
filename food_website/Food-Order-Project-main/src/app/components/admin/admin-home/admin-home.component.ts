808298b3e298f82c55710ae6ad8c1c0c0b0bd61f
Javidm77 <Javidpasham77@gmail.com>
2024-05-01 11:45:05 +0530

    added

---
+++ food_website/Food-Order-Project-main/src/app/components/admin/admin-home/admin-home.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FoodorderService } from '../../service/foodorder.service';
+
+@Component({
+  selector: 'app-admin-home',
+  templateUrl: './admin-home.component.html',
+  styleUrls: ['./admin-home.component.scss']
+})
+export class AdminHomeComponent implements OnInit {
+  userName:string='';
+
+  constructor(
+    private fService:FoodorderService
+  ){
+    if(this.fService.getAdminName() !==null){
+      this.userName=this.fService.getAdminName();
+    }
+    this.fService.isAdminLoginPresent();
+  }
+  ngOnInit(): void {
+    
+  }
+  tasks: string[] = ['Review reports', 'Send emails', 'Schedule meetings', 'Prepare presentation'];
+
+}
